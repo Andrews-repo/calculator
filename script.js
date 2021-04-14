8ef84ec5ed0cccece56c7daca5681968b394809d
Andrews-repo <andrewpatty3@gmail.com>
2021-04-14 13:14:35 -0700

    s

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 //basic math functions
 function add(a, b) {
     return a + b
@@ -18,4 +19,24 @@ function divide(a, b) {
 
 function percent(a) {
     return a / 100 
+}
+
+//function for all of the math functions
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "−":
+            return substract(a, b);
+        case "×":
+            return multiply(a, b);
+        case "÷":
+            if (b === 0) return null;
+            else return divide(a, b);
+        case "%":
+            return percent(a, b);
+        default:
+            return null;
 }
