219631fab2dc2cc4968899322e39fccb7219ba98
Andrews-repo <andrewpatty3@gmail.com>
2021-04-13 18:14:03 -0700

    cleard

---
+++ script.js
@@ -0,0 +1,21 @@
+
+//basic math functions
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function mult(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+   return a / b
+}
+
+function percent(a) {
+    return a / 100 
+}
